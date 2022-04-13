98b64f64a9a06a8a6733dc450443b91e9b18b627
oliveira.bruno <oliveira.bruno@greenbeltti.onmicrosoft.com>
2022-04-12 21:18:57 -0300

    feat: Desenvolvido config multer

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const multer = require('multer');
+const FileController = require('./controller/FileController');
+
+const multerConfig = require('./config/multer');
 
 const indexRouter = require('./routes/index');
 const legendariesRouter = require('./routes/legendaries');
@@ -13,6 +17,8 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+const uploadFile = multer({ storage: multerConfig});
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -21,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/legendaries', legendariesRouter);
+app.post('/files', uploadFile.single('file'), FileController.storeFile);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
