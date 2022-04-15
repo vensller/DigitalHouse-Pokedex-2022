abb8eb00d5de446269a40211435640bd7f0f34b3
Ivens Diego Muller <ivensdiegomuller@gmail.com>
2022-04-14 21:42:07 -0300

    Add login page and redirect when user is not authenticated

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,8 +1,10 @@
 function isAuthenticated(request, response, next) {
   if (!request.headers.authorization) {
-    return response.status(401).json({
-      error: 'User not authenticated'
-    });
+    // return response.status(401).json({
+    //   error: 'User not authenticated'
+    // });
+    // Código para redirecionar o usuário para a tela de login
+    return response.redirect('/login');
   }  
   next();
 }
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const auth = require('../middlewares/auth');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', auth, function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/login', (req, res) => {
+  res.render('login');
+})
 
 module.exports = router;
